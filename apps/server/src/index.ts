bb4c60df9ff8d14297a0200f83f36949aa0c26b9
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2024-02-15 01:55:39 +0530

    RTC succesfull!

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,19 +8,35 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 const port = process.env.PORT || 3001;
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    name: "vanshika",
+    name: "Anirudh Singh Bhadauria",
     age: 21,
   });
 });
 
 io.on("connection", (socket) => {
-  socket.on("ping", ({ name, message }) => {
-    console.log(`${name}: ${message}`);
+  // Listners!
+  // socket.on("ping", ({ name, message }) => {
+  //   console.log(`${name}: ${message}`);
+  // });
+
+  socket.on("SendMessage", (messageData) => {
+    // console.log(`${socket.id}: ${message}`);
+
+    io.emit("ReciveMessage", messageData);
   });
+
+  // Emitters!
+
+  // A new User joins!
+  io.emit("UserJoins", { socketName: socket.id });
 });
 
 server.listen(port, () => {
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,56 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { FormEvent, useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080");
-
 const Homepage = () => {
-  socket.emit("ping", {
-    name: "anirudh",
-    message: "hello from client!",
-  });
+  const socket = useMemo(() => io("http://localhost:8080"), []);
 
-  return <div>Homepage</div>;
+  const [message, setMessage] = useState("");
+
+  const submitHandeler = (event: FormEvent) => {
+    event.preventDefault();
+    // console.log("submitted!");
+    socket.emit("SendMessage", { id: socket.id, message });
+  };
+
+  useEffect(() => {
+    // Listners!
+    socket.on("UserJoins", ({ socketName }) => {
+      console.log(`New User Joined!, Hello, I am ${socketName}`);
+    });
+
+    // Reciveing message!
+    socket.on("ReciveMessage", ({ id, message }) =>
+      console.log(`${id}: ${message}`)
+    );
+
+    // Emmiters!
+    // socket.emit("ping", {
+    //   name: "anirudh",
+    //   message: "hello from client!",
+    // });
+  }, []);
+
+  return (
+    <form
+      onSubmit={submitHandeler}
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <input
+        onChange={(event) => setMessage(event.target.value)}
+        type="text"
+        value={message}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
 };
 
 export default Homepage;
